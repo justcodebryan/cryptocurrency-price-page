39cabec9af11ce8446ee3fa0920c4e8e51d6e619
justcodebryan <huangjifan1819@163.com>
2023-02-10 11:51:33 +0800

    feat: update card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren } from 'react'
 import styles from './styles.module.scss'
 
 type CardProps = PropsWithChildren
+
 const Card = ({ children }: CardProps) => {
   return <div className={styles['card-wrapper']}>{children}</div>
 }
